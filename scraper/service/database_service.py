73869a63e322b2d1a9c7042bebdfcc67307f2a2a
rafal.kadzevic <rafal.kadzevic@cec.valantic.com>
2025-05-19 09:31:16 +0300

    Fix capacity relation

--- scraper/service/database_service.py
+++ scraper/service/database_service.py
@@ -19,14 +19,14 @@ def insert_brand(cursor, brand):
     if brand in brand_cache:
         return brand_cache[brand]
 
-    cursor.execute("SELECT id FROM brands WHERE name=%s", (brand))
+    cursor.execute("SELECT id FROM brands WHERE name=%s", (brand,))
     result = cursor.fetchone()
     if result:
         brand_cache[brand] = result['id']
 
         return result['id']
 
-    cursor.execute("INSERT INTO brands (name) VALUES (%s)", (brand))
+    cursor.execute("INSERT INTO brands (name) VALUES (%s)", (brand,))
     brand_id = cursor.lastrowid
     brand_cache[brand] = brand_id
 
@@ -36,14 +36,14 @@ def insert_model(cursor, model_name, brand_id):
     if model_name in model_cache:
         return model_cache[model_name]
 
-    cursor.execute("SELECT id FROM models WHERE name=%s", (model_name))
+    cursor.execute("SELECT id FROM models WHERE name=%s", (model_name,))
     result = cursor.fetchone()
     if result:
         model_cache[model_name] = result['id']
 
         return result['id']
 
-    cursor.execute("INSERT INTO models (name, brand_id) VALUES (%s, %s)", (model_name,brand_id))
+    cursor.execute("INSERT INTO models (name, brand_id) VALUES (%s, %s)", (model_name,brand_id,))
     model_id = cursor.lastrowid
     model_cache[model_name] = model_id
 
@@ -53,26 +53,26 @@ def insert_capacity(cursor, capacity):
     if capacity in capacity_cache:
         return capacity_cache[capacity]
 
-    cursor.execute("SELECT id FROM capacities WHERE size=%s", (capacity))
+    cursor.execute("SELECT id FROM capacities WHERE size=%s", (capacity,))
     result = cursor.fetchone()
     if result:
         capacity_cache[capacity] = result['id']
 
         return result['id']
 
-    cursor.execute("INSERT INTO capacities (size) VALUES (%s)", (capacity))
+    cursor.execute("INSERT INTO capacities (size) VALUES (%s)", (capacity,))
     capacity_id = cursor.lastrowid
     capacity_cache[capacity] = capacity_id
 
     return capacity_id
 
 def insert_category(cursor, category_name):
-    cursor.execute("SELECT id FROM categories WHERE name=%s", (category_name))
+    cursor.execute("SELECT id FROM categories WHERE name=%s", (category_name,))
     result = cursor.fetchone()
     if result:
         return result['id']
 
-    cursor.execute("INSERT INTO categories (name) VALUES (%s)", (category_name))
+    cursor.execute("INSERT INTO categories (name) VALUES (%s)", (category_name,))
 
     return cursor.lastrowid
 
@@ -80,10 +80,10 @@ def insert_item(cursor, model_id, category_id, capacity_id, item):
     cursor.execute("""
         INSERT INTO items (model_id, category_id, price, item_hash, grade, capacity_id)
         VALUES (%s, %s, %s, %s, %s, %s)
-    """, (model_id, category_id, item['price'], item['hash'], item['grade'], capacity_id))
+    """, (model_id, category_id, item['price'], item['hash'], item['grade'], capacity_id,))
 
 def item_exists(cursor, item_hash: str) -> bool:
-    cursor.execute("SELECT 1 FROM items WHERE item_hash = %s LIMIT 1", (item_hash))
+    cursor.execute("SELECT 1 FROM items WHERE item_hash = %s LIMIT 1", (item_hash,))
 
     return cursor.fetchone() is not None
 
